82c3512ae17d3c2b690128795eeedc26018dfd03
skynyrd <anil.surmeli@gmail.com>
2016-10-31 00:28:29 +0300

    adds manage courses page and related components

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,23 +3,27 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component{
   constructor(props){
     super(props);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
-  courseRow(course, index){
-    return <div key={index}>{course.title}</div>;
+  redirectToAddCoursePage(){
+    browserHistory.push('/course');
   }
 
   render(){
-    const {courses} = this.props;
-
     return(
       <div>
         <h1>Courses</h1>
-        <CourseList courses={courses}/>
+        <input type="submit"
+               value="Add Course"
+               className="btn btn-primary"
+               onClick={this.redirectToAddCoursePage}/>
+        <CourseList courses={this.props.courses}/>
       </div>
     );
   }
